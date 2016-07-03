24b1feed1e100eb4d50f0f227d2225afcf187205
adrianmarino <adrianmarino@gmail.com>
2016-07-03 01:09:51 -0300

    todo list component

--- client/src/component/app.js
+++ client/src/component/app.js
@@ -3,6 +3,7 @@
 //-----------------------------------------------------------------------------
 import { Component, Inject } from '@angular/core';
 import { NavBarComponent } from './navbar';
+import { TodoListComponent } from './todolist';
 import { TodoService } from '../service/todo';
 //-----------------------------------------------------------------------------
 //
@@ -14,19 +15,12 @@ import { TodoService } from '../service/todo';
 //-----------------------------------------------------------------------------
 @Component({
   selector: 'app',
-  directives: [NavBarComponent],
+  directives: [NavBarComponent, TodoListComponent],
   template: `
     <div class="container">
       <navbar title="TODOS" phase="Simple todo crud" icon="glyphicon glyphicon-ok"></navbar>
       <div class="well well-lg">
-        <ul class="list-group">
-          <li *ngFor="let todo of todos; let number=index" 
-              class="list-group-item
-                     list-group-item-{{ todo.isCompleted ? 'success' : 'warning'}}">
-             <span class="badge">{{number + 1}}</span>
-            {{todo.text}}
-          </li>
-        </ul>
+        <todolist [list]="todos"></todolist>
       </div>
     </div>
   `
---
+++ client/src/component/todolist.js
@@ -0,0 +1,28 @@
+//-----------------------------------------------------------------------------
+// Require
+//-----------------------------------------------------------------------------
+import { Component } from '@angular/core';
+//-----------------------------------------------------------------------------
+//
+//
+//
+//
+//-----------------------------------------------------------------------------
+// Component
+//-----------------------------------------------------------------------------
+@Component({
+  selector: 'todolist',
+  inputs: ['list'],
+  template: `
+    <ul class="list-group">
+        <li *ngFor="let todo of list; let number=index" 
+            class="list-group-item
+                    list-group-item-{{ todo.isCompleted ? 'success' : 'warning'}}">
+            <span class="badge">{{number + 1}}</span>
+            {{todo.text}}
+        </li>
+    </ul>
+  `
+})
+export class TodoListComponent {
+}
