98f8c65ee53ef32de262292da917faf562c26346
adrianmarino <adrianmarino@gmail.com>
2016-07-03 20:06:53 -0300

    add author component

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -5,6 +5,7 @@ import { Component, Inject } from '@angular/core';
 import { CoverTemplateComponent } from './lib/template/cover';
 import { TodoListComponent } from './todolist';
 import { IconComponent } from './lib/icon'
+import { AuthorComponent } from './lib/author'
 import { TodoService } from '../services/todo';
 //-----------------------------------------------------------------------------
 //
@@ -16,18 +17,15 @@ import { TodoService } from '../services/todo';
 //-----------------------------------------------------------------------------
 @Component({
   selector: 'app',
-  directives: [CoverTemplateComponent, IconComponent, TodoListComponent],
+  directives: [CoverTemplateComponent, IconComponent, AuthorComponent, TodoListComponent],
   template: `
     <cv-template>
-
       <cv-title>
         TODOS <icon name="ok"></icon>
       </cv-title>
-
       <cv-menu>
         <a class="nav-link active" href="#">Sign In</a>
       </cv-menu>
-
       <cv-content>
         <div class="lead" style="text-align-last: left">
           <a href="#" class="btn btn-lg btn-secondary">
@@ -38,11 +36,9 @@ import { TodoService } from '../services/todo';
           <todolist [list]="todos"></todolist>
         </div>
       </cv-content>
-
       <cv-footer>
-        <p>Writed by <a href="#">Adrian Norberto Marino</a>. (adrianmarino@gmail.com)</p>
+        <author name="Adrian Norberto Marino" email="adrianmarino@gmail.com" github="adrianmarino"></author>
       </cv-footer>
-
     </cv-template>
   `
 })
